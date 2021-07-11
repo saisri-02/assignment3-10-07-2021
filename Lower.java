9a5dc7c36f70bde4eed9686359ca9fb8f2d3ce71
saisri-02 <71486510+saisri-02@users.noreply.github.com>
2021-07-11 14:15:57 +0530

    initial commit

---
+++ Lower.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Lower
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter String : ");
+String str=sc.nextLine();
+System.out.println("Given string : "+str);
+String low=str.toLowerCase();
+System.out.println("Lowercase string : "+low);
+}
+}
+
+
+Output:-
+
+Enter String : 
+HansIthA
+Given string : HansIthA
+Lowercase string : hansitha
